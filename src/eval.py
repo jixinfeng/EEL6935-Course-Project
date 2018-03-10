cb1f389b3ac9c7fbbb9c963c71d39bd93019cea5
Caleb Bryant <cal2u@ufl.edu>
2018-03-09 21:47:56 -0500

    Fix bug with parsing data, improve class design

--- src/eval.py
+++ src/eval.py
@@ -5,8 +5,10 @@ from model import LogisticRegression
 
 VOCAB_SIZE = 89526
 TESTING_BOW = "aclImdb/test/labeledBow.feat"
+TRAINING_BOW = "aclImdb/train/labeledBow.feat"
 
-def process_data(vocab_size, class_ix):
+def process_data(vocab_size, class_type):
+    class_ix = {str(rating): rating - 1 for rating in range(1,11)}
 
     # Produces y vectors that look like [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
     # Produces x vectors that look like [0, 1, 1, 0, 1, 0, 0, 0, 1, 0, ...]
@@ -15,7 +17,6 @@ def process_data(vocab_size, class_ix):
         class_, bow = tokens[0], tokens[1:]
 
         x = np.zeros(vocab_size, np.int8)
-        y = np.zeros(len(class_ix.keys()), np.int8)
 
         # Mark every word appearing in the document (movie review)
         for word in bow:
@@ -23,27 +24,45 @@ def process_data(vocab_size, class_ix):
             index = int(index) - 1
             x[index] = 1
 
-        # Mark which class the document belongs to
-        y[class_ix[class_]] = 1
+        if class_type == 'multiclass':
+            y = np.zeros(len(class_ix.keys()), np.int8)
+            # Mark which class the document belongs to
+            y[class_ix[class_]] = 1
+
+        if class_type == 'binary':
+            y = np.zeros(2, np.int8)
+            # Mark which class the document belongs to
+            y[0 if class_ix[class_] <= 5 else 1] = 1
 
         return x, y
 
     return lambda line: tf.py_func(encode_one_hot_vectors, [line], [tf.int8, tf.int8])
 
 
-def input_fn():
+def test_input_fn(type='binary'):
     # Map the movie ratings (strings) to an array index
-    class_ix = {str(rating): rating - 1 for rating in range(1,11)}
     dataset = tf.data.TextLineDataset(TESTING_BOW)
-    dataset = dataset.map(process_data(VOCAB_SIZE, class_ix), num_parallel_calls=2)
+    dataset = dataset.map(process_data(VOCAB_SIZE, type), num_parallel_calls=1)
     return dataset
 
+def train_input_fn(type='binary'):
+    # Map the movie ratings (strings) to an array index
+    dataset = tf.data.TextLineDataset(TRAINING_BOW)
+    dataset = dataset.map(process_data(VOCAB_SIZE, type), num_parallel_calls=1)
+    return dataset
 
 def main():
     # Create a logistic regression model,
     # and train on the Stanford Movie Review Dataset
-    model = LogisticRegression()
-    model.binary_accuracy(input_fn, batch_size=50)
+    model = LogisticRegression(model_dir="/tmp/model.ckpt")
+
+    print("Training accuracy")
+    model.score(train_input_fn, batch_size=50)
+
+    print("Testing accuracy")
+    model.score(test_input_fn, batch_size=50)
+
 
 if __name__ == "__main__":
     main()
+
--- src/model.py
+++ src/model.py
@@ -1,19 +1,18 @@
-import numpy as np
 import tensorflow as tf
+import datetime
 
 # Built referencing https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/2_BasicModels/logistic_regression.py
+# and https://web.stanford.edu/class/cs20si/2017/lectures/notes_03.pdf
+
 class LogisticRegression:
     def __init__(self, model_dir=None):
         self.width_in = None
         self.width_out = None
         self.model_dir = model_dir
-        self.sess = None
 
-    def _get_widths(self, iterator):
+    def _get_widths(self, dataset):
         # Find out the input and output data dimensions
-        # (Note: This information could also be passed to the constructor.
-        # I'm not sure how long it takes for tensorflow to retrieve these
-        # values.)
+        iterator = dataset.make_one_shot_iterator()
         next_element = iterator.get_next()
 
         with tf.Session() as sess:
@@ -21,7 +20,6 @@ class LogisticRegression:
             # Retrieve network depth, # of output classes
             self.width_in = batch_xs.shape[0]
             self.width_out = batch_ys.shape[0]
-
             print("Width in: %d, width out: %d" % (self.width_in, self.width_out))
 
     # Returns a tensorflow variable containing the loss function
@@ -33,158 +31,102 @@ class LogisticRegression:
 
         with tf.name_scope('weights'):
             # Set model weights
-            W = tf.Variable(tf.zeros([self.width_in, self.width_out]))
+            self.W = tf.get_variable("W", shape=(self.width_in, self.width_out), initializer=tf.contrib.layers.xavier_initializer())
 
         with tf.name_scope('biases'):
-            b = tf.Variable(tf.zeros([self.width_out]))
+            self.b = tf.get_variable("b", shape=self.width_out, initializer=tf.contrib.layers.xavier_initializer())
+
+        with tf.name_scope('logits'):
+            self.logits = tf.matmul(self.x, self.W) + self.b
 
         with tf.name_scope('softmax'):
-            # Calculate prediction
-            pred = tf.nn.softmax(tf.matmul(self.x, W) + b)
-            self.maxpred = tf.argmax(pred, 1)
+            preds = tf.nn.softmax(self.logits)
+            self.maxpreds = tf.argmax(preds, 1)
 
         with tf.name_scope('loss'):
             # Minimize error using cross entropy
-            loss = tf.reduce_mean(-tf.reduce_sum(self.y * tf.log(pred), reduction_indices=1))
+            loss = tf.reduce_mean(-tf.reduce_sum(self.y * tf.log(preds), reduction_indices=1))
             tf.summary.scalar('loss_summary', loss)
             return loss
 
     # Train the model with the Dataset passed in
-    def train(self, input_fn, num_epochs=25, learning_rate=0.0001, batch_size=20, display_period=100):
-        # Prepare the dataset epochs/batches using the method suggested at
-        # https://stackoverflow.com/questions/47410778/epoch-counter-with-tensorflow-dataset-api
-        # to keep track of the epoch number. It's not pretty, but it'll work for now.
+    def train(self, input_fn, save_dir=None, num_epochs=25, learning_rate=0.0001, batch_size=20, display_period=100):
         dataset = input_fn()
-        self._get_widths(dataset.make_one_shot_iterator())
 
-        epoch_counter = tf.data.Dataset.range(num_epochs)
-        dataset = epoch_counter.flat_map(lambda i: tf.data.Dataset.zip(
-            (tf.data.Dataset.from_tensors(i).repeat(), dataset)))
-        dataset = dataset.apply(tf.contrib.data.batch_and_drop_remainder(batch_size))
-
-        # Get an iterator to the dataset
-        iterator = dataset.make_one_shot_iterator()
-        next_element = iterator.get_next()
+        # Get info on the dataset
+        self._get_widths(dataset)
 
         # Prepare our model and acquire a reference to the loss
         loss = self._build_model()
+
+        # Batch the data
+        dataset = dataset.shuffle(buffer_size=25000)
+        dataset = dataset.apply(tf.contrib.data.batch_and_drop_remainder(batch_size))
+
+        # Get an iterator to the dataset
+        iterator = dataset.make_initializable_iterator()
+        next_element = iterator.get_next()
+
+        # Initialize all of the model variables (i.e. assign their default values)
+        init = tf.global_variables_initializer()
 
         # Create an optimization variable
         with tf.name_scope('optimizer'):
             train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
 
-        # Initialize all of the model variables (i.e. assign their default values)
-        init = tf.global_variables_initializer()
-
-        saver = tf.train.Saver()
-
-        import datetime
-
         # Start training
         with tf.Session() as sess:
+            # Load the previous model if it was listed in the constructor
+            saver = tf.train.Saver()
+            if self.model_dir:
+                saver.restore(sess, self.model_dir)
+                self.model_dir = None
+
+            # Init graph variables and set up logging
             merged = tf.summary.merge_all()
             train_writer = tf.summary.FileWriter('./logs/train/%s' % datetime.datetime.now(), sess.graph)
             sess.run(init)
 
-            running_cost = 0.0
-            batch_num = 0
-            epoch = 0
-            i = 0
-            # Keep training as long as we have data
-            while True:
-                try:
-                    # Note that each x, y pair in the batch has an epoch number
-                    epochs, (batch_xs, batch_ys) = sess.run(next_element)
+            step_num = 0
+            for epoch in range(num_epochs):
+                sess.run(iterator.initializer)
+                batch_num = 1
+                running_cost = 0.0
 
-                    # Reset batch numbers at the beginning of a new epoch
-                    if epochs[0] != epoch:
-                        batch_num = 1
-                    batch_num += 1
-                    epoch = epochs[0]
-
-                except tf.errors.OutOfRangeError:
-                    print("End of training dataset.")
-                    break
-
-                # Train using batch data
-                summary, c, _ = sess.run([merged, loss, train_step], feed_dict={self.x: batch_xs,
-                                                              self.y: batch_ys})
-
-                # Accumulate loss
-                running_cost += c
-
-                # Display log message every n batches
-                if batch_num % display_period == 0:
-                    print("Epoch: %04d Batch: %06d cost=%.9f" % (epoch + 1, batch_num, running_cost/display_period))
-                    running_cost = 0.0
-
-                train_writer.add_summary(summary, i)
-                i += 1
-
-            print("Training Finished!")
-            save_path = saver.save(sess, "/tmp/model.ckpt")
-
-    def binary_accuracy(self, input_fn, batch_size=20, display_period=100):
-        tf.reset_default_graph()
-        dataset = input_fn()
-        self._get_widths(dataset.make_one_shot_iterator())
-        self._build_model()
-
-        dataset = dataset.apply(tf.contrib.data.batch_and_drop_remainder(batch_size))
-        print("Dataset size: " + str(dataset.output_shapes))
-        iterator = dataset.make_one_shot_iterator()
-        next_element = iterator.get_next()
-
-        saver = tf.train.Saver()
-
-        subtotal = 0
-        total_tested = 0
-
-        if self.sess is None:
-            with tf.Session() as sess:
-                saver.restore(sess, "/tmp/model.ckpt")
-                print("Model restored.")
-
-                batch_num = 0
-                five = tf.constant(5, tf.int64)
-
-                import timeit
-                start = timeit.default_timer()
-
+                # Run through the epoch
                 while True:
                     try:
-                        pred_negative = tf.less_equal(self.maxpred, five)
-                        real_negative = tf.less_equal(tf.argmax(self.y, 1), five)
-
-                        correct_prediction = tf.equal(pred_negative, real_negative)
-
-                        # Calculate number correct
-                        correct = tf.reduce_sum(tf.cast(correct_prediction, tf.float32))
-
-                        xs, ys = sess.run(next_element)
-
-                        total_tested += xs.shape[0]
-
-                        subtotal += correct.eval({self.x: xs, self.y: ys})
-
-                        if batch_num % display_period == 0:
-                            print("Batch: %06d %d/%d" % (batch_num, subtotal, total_tested))
-
-                        batch_num += 1
+                        # Note that each x, y pair in the batch has an epoch number
+                        batch_xs, batch_ys = sess.run(next_element)
 
                     except tf.errors.OutOfRangeError:
-                        print("End of dataset.")
+                        print("End of training dataset.")
                         break
 
-                stop = timeit.default_timer()
+                    # Train using batch data
+                    summary, c, _ = sess.run([merged, loss, train_step], feed_dict={self.x: batch_xs,
+                                                                                    self.y: batch_ys})
+                    # Accumulate loss
+                    running_cost += c
 
-                print("Finished evaluation: %.5f%%" % (subtotal / total_tested))
-                print(stop - start)
+                    # Display log message every n batches
+                    if batch_num % display_period == 0:
+                        print("Epoch: %04d Batch: %06d cost=%.9f" % (epoch + 1, batch_num, running_cost / display_period))
+                        running_cost = 0.0
 
-    def class_accuracy(self, input_fn, batch_size=20, display_period=100):
+                    train_writer.add_summary(summary, step_num)
+                    batch_num += 1
+                    step_num += 1
+
+            print("Training Finished!")
+            if save_dir:
+                save_path = saver.save(sess, save_dir)
+                print("Saved model to "+save_dir)
+
+    def score(self, input_fn, batch_size=20, display_period=100):
         tf.reset_default_graph()
         dataset = input_fn()
-        self._get_widths(dataset.make_one_shot_iterator())
+        self._get_widths(dataset)
         self._build_model()
 
         dataset = dataset.apply(tf.contrib.data.batch_and_drop_remainder(batch_size))
@@ -192,44 +134,43 @@ class LogisticRegression:
         iterator = dataset.make_one_shot_iterator()
         next_element = iterator.get_next()
 
-        saver = tf.train.Saver()
-
         subtotal = 0
         total_tested = 0
 
-        if self.sess is None:
-            with tf.Session() as sess:
-                saver.restore(sess, "/tmp/model.ckpt")
+        with tf.Session() as sess:
+            if self.model_dir:
+                saver = tf.train.Saver()
+                saver.restore(sess, self.model_dir)
                 print("Model restored.")
 
-                batch_num = 0
+            batch_num = 0
 
-                import timeit
-                start = timeit.default_timer()
+            import timeit
+            start = timeit.default_timer()
 
-                while True:
-                    try:
-                        correct_prediction = tf.equal(self.maxpred, tf.argmax(self.y, 1))
+            while True:
+                try:
+                    correct_preds = tf.equal(self.maxpreds, tf.argmax(self.y, 1))
 
-                        # Calculate accuracy
-                        correct = tf.reduce_sum(tf.cast(correct_prediction, tf.float32))
+                    # Calculate accuracy
+                    correct = tf.reduce_sum(tf.cast(correct_preds, tf.float32))
 
-                        xs, ys = sess.run(next_element)
+                    xs, ys = sess.run(next_element)
 
-                        total_tested += xs.shape[0]
+                    total_tested += xs.shape[0]
 
-                        subtotal += correct.eval({self.x: xs, self.y: ys})
+                    subtotal += correct.eval({self.x: xs, self.y: ys})
 
-                        if batch_num % display_period == 0:
-                            print("Batch: %06d %d/%d" % (batch_num, subtotal, total_tested))
+                    if batch_num % display_period == 0:
+                        print("Batch: %06d %d/%d" % (batch_num, subtotal, total_tested))
 
-                        batch_num += 1
+                    batch_num += 1
 
-                    except tf.errors.OutOfRangeError:
-                        print("End of dataset.")
-                        break
+                except tf.errors.OutOfRangeError:
+                    print("End of dataset.")
+                    break
 
-                stop = timeit.default_timer()
+            stop = timeit.default_timer()
 
-                print("Finished evaluation: %.5f%%" % (subtotal/total_tested))
-                print(stop - start)
+        print("Finished evaluation: %.5f%%" % (subtotal/total_tested))
+        print(stop - start)
