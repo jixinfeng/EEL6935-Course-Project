0c6c3491410427d2d2a9866e08086e8b8d7fb1e6
Jixin Feng <ufjfeng@users.noreply.github.com>
2018-04-11 15:43:40 -0400

    add web interface

---
+++ src/Flask.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        form_data = request.form
+        display_data = {"Model": form_data["model"],
+                        "Review": form_data["review"],
+                        "Granularity": form_data["granularity"]}
+        return render_template("result.html", result=display_data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
