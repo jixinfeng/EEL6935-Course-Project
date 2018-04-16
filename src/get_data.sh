c07b3614eff18a6b3b66213d87da59848ee25481
Jixin Feng <ufjfeng@users.noreply.github.com>
2018-04-16 15:38:43 -0400

    download data into data folder

--- src/get_data.sh
+++ src/get_data.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Download Stanford Large Movie Review Dataset
+mkdir data
+cd data
 wget http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
 tar -xvf aclImdb_v1.tar.gz
 rm aclImdb_v1.tar.gz
