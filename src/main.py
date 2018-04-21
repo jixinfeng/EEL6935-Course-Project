64199eb0e9d4db071a94cd5f39838228b485f576
Caleb Bryant <cal2u@ufl.edu>
2018-04-21 15:00:24 -0400

    update logistic regression constructor

--- src/main.py
+++ src/main.py
@@ -1,50 +1,52 @@
 import argparse
+import numpy as np
 from dataset import *
 from models.lstm import LSTM
-from models.logreg import LogisticRegression
+from models.logreg import LogisticRegression, LRConfig
 
 
 def get_model(args):
     if args.model == 'lstm':
+        args.word_dict = np.load(args.dataset[:-4]+"_dict.npy").item()
+        args.num_classes = 10
         model = LSTM(args)
     else:
-        model = LogisticRegression(args)
+        vocab_file = 'data/logreg/imdb.vocab'
+        model = LogisticRegression(LRConfig(width_out=10, vocab_file=vocab_file), model_dir=args.model_dir)
 
     return model
 
 
 def get_dataset(args):
     if args.model == 'lstm':
-        dataset = bow_dataset(args.dataset)
-    else:
         dataset = numpy_dataset(args.dataset)
-
+    else:
+        dataset = bow_dataset(args.dataset)
     return dataset
 
 
 def main():
     parser = argparse.ArgumentParser(description='Movie Review Classifier')
-    parser.add_argument('-dataset', type=str, default='dataset/dataset_full.npz', help='dataset to use')
+    parser.add_argument('model', type=str, help='model to use. e.g. lstm, lonreg')
+    parser.add_argument('dataset', type=str, help='dataset to use')
+
     parser.add_argument('-lr', type=float, default=0.001, help='learning rate')
     parser.add_argument('-epochs', type=int, default=10, help='number of epochs for train [default: 10]')
     parser.add_argument('-batch-size', type=int, default=1, help='batch size')
     parser.add_argument('-hidden-size', type=int, default=200, help='size of hidden state')
     parser.add_argument('-max-timesteps', type=int, default=200, help='length of sequence tracked')
     parser.add_argument('-embedding-dim', type=int, default=100, help='batch size')
-    parser.add_argument('-log-interval', type=int, default=1, help='logging rate')
     parser.add_argument('-vocab-size', type=int, default=87798, help='number of words to embed')
     parser.add_argument('-save-dir', type=str, default='snapshot', help='where to save the snapshot')
-    parser.add_argument('-num_classes', type=int, default=10, help='number of classes being predicted')
-    parser.add_argument('-model-dir', type=str, default=None, help='filename of model snapshot [default: None]')
-    parser.add_argument('-predict', type=str, default='', help='predict the results for the dataset')
+    parser.add_argument('-model-dir', type=str, default=None, help='filename of model  [default: None]')
+    parser.add_argument('-predict', type=str    , default='', help='predict the results for the dataset')
     parser.add_argument('-test', action='store_true', default=False, help='train or test')
-    parser.add_argument('-display-interval', type=int, default=100, help='how many steps to wait before testing [default: 100]')
+    parser.add_argument('-log-interval', type=int, default=1, help='logging rate for tensorboard')
+    parser.add_argument('-display-interval', type=int, default=1, help='logging for terminal messages')
     parser.add_argument('-test-interval', type=int, default=100, help='how many steps to wait before testing [default: 100]')
     parser.add_argument('-save-interval', type=int, default=500, help='how many steps to wait before saving [default:500]')
-    parser.add_argument('-model', type=str, default="lstm", help='model to use. e.g. lstm, linreg')
 
     args = parser.parse_args()
-
     model = get_model(args)
 
     if args.predict is not '':
--- src/models/logreg.py
+++ src/models/logreg.py
@@ -1,50 +1,69 @@
 import tensorflow as tf
 import datetime
-import os
 import numpy as np
+from collections import defaultdict
 from nltk import word_tokenize
 
 # Built referencing https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/2_BasicModels/logistic_regression.py
 # and https://web.stanford.edu/class/cs20si/2017/lectures/notes_03.pdf
 
+class LRConfig():
+    def __init__(self, width_out, width_in=None, vocab_file=None):
+        assert width_in or vocab_file, "Either width_in or vocab_file must be given"
+        self.width_out = width_out
+        self.word_dict = None
+
+        if vocab_file:
+            # Load the vocab file if given
+            self.word_dict = defaultdict(int)
+            with open(vocab_file, encoding='utf-8') as f:
+                for linenum, line in enumerate(f.readlines()):
+                    self.word_dict[line[:-1]] = linenum
+            # Safety check
+            if width_in:
+                assert width_in == linenum, "Error: width_in does not match size of vocab_file"
+            self.width_in = linenum
+        else:
+            self.width_in = width_in
+
 class LogisticRegression:
-    def __init__(self, args):
-        self.width_in = None
-        self.width_out = None
-        self.model_dir = args.model_dir
+    def __init__(self, config, model_dir=None, sess=None):
+        self.config = config
+        self.model_dir = model_dir
 
-        self.word_dict = {}
-        vocab_file = os.path.join(os.path.dirname(__file__), '../data/aclImdb/imdb.vocab')
-        with open(vocab_file, encoding='utf-8') as f:
-            linenum = 0
-            for line in f.readlines():
-                self.word_dict[line[:-1]] = linenum
+        # Create a tf session if needed
+        if sess is None:
+            self.sess = tf.Session()
+        else:
+            self.sess = sess
 
-    def _get_widths(self, dataset):
-        # Find out the input and output data dimensions
-        iterator = dataset.make_one_shot_iterator()
-        next_element = iterator.get_next()
+        # Load the model if one is passed in
+        if self.model_dir:
+            self.loss = self._build_model(config)
+            saver = tf.train.Saver()
+            saver.restore(sess, self.model_dir)
+            print("Model restored.")
+        else:
+            self.loss = None
 
-        with tf.Session() as sess:
-            batch_xs, batch_ys = sess.run(next_element)
-            # Retrieve network depth, # of output classes
-            self.width_in = batch_xs.shape[0]
-            self.width_out = batch_ys.shape[0]
-            print("Width in: %d, width out: %d" % (self.width_in, self.width_out))
+    def __del__(self):
+        self.close_sess()
 
-    # Returns a tensorflow variable containing the loss function
-    def _build_model(self):
+    # Return a tensorflow variable containing the loss function
+    def _build_model(self, config):
+        assert hasattr(config, 'width_in') and hasattr(config, 'width_out'), "Missing width_in or width_out from config"
+
         with tf.name_scope('input'):
             # Input data
-            self.x = tf.placeholder(tf.float32, shape=[None, self.width_in])
-            self.y = tf.placeholder(tf.float32, shape=[None, self.width_out])
+            self.x = tf.placeholder(tf.float32, shape=[None, config.width_in])
+            self.y = tf.placeholder(tf.float32, shape=[None, config.width_out])
 
         with tf.name_scope('weights'):
             # Set model weights
-            self.W = tf.get_variable("W", shape=(self.width_in, self.width_out))
+            self.W = tf.get_variable("W", shape=(config.width_in, config.width_out))
 
         with tf.name_scope('biases'):
-            self.b = tf.get_variable("b", shape=self.width_out)
+            self.b = tf.get_variable("b", shape=config.width_out)
 
         with tf.name_scope('logits'):
             self.logits = tf.matmul(self.x, self.W) + self.b
@@ -59,15 +78,21 @@ class LogisticRegression:
             tf.summary.scalar('loss_summary', loss)
             return loss
 
-    # Train the model with the Dataset passed in
+    # Train the model with the tf.Dataset returned by @param input_fn
     def train(self, input_fn, args):
+        # Create a tf session if needed
+        if self.sess is None:
+            self.sess = tf.Session()
+
         dataset = input_fn()
 
-        # Get info on the dataset
-        self._get_widths(dataset)
+        if self.loss is None:
+            # Prepare our model and acquire a reference to the loss
+            self.loss = self._build_model(self.config)
 
-        # Prepare our model and acquire a reference to the loss
-        loss = self._build_model()
+            # Initialize all of the model variables (i.e. assign their default values)
+            init = tf.global_variables_initializer()
+            self.sess.run(init)
 
         # Batch the data
         dataset = dataset.shuffle(buffer_size=25000)
@@ -79,65 +104,65 @@ class LogisticRegression:
 
         # Create an optimization variable
         with tf.name_scope('optimizer'):
-            train_step = tf.train.AdamOptimizer(args.lr).minimize(loss)
+            train_step = tf.train.AdamOptimizer(args.lr).minimize(self.loss)
 
-        # Initialize all of the model variables (i.e. assign their default values)
-        init = tf.global_variables_initializer()
+        # Init graph variables and set up logging
+        merged = tf.summary.merge_all()
+        train_writer = tf.summary.FileWriter('./logs/train/%s' % datetime.datetime.now(), self.sess.graph)
 
-        # Start training
-        with tf.Session() as sess:
-            # Load the previous model if it was listed in the constructor
-            saver = tf.train.Saver()
-            if self.model_dir:
-                saver.restore(sess, self.model_dir)
-                self.model_dir = None
+        saver = tf.train.Saver()
+        step_num = 0
 
-            # Init graph variables and set up logging
-            merged = tf.summary.merge_all()
-            train_writer = tf.summary.FileWriter('./logs/train/%s' % datetime.datetime.now(), sess.graph)
-            sess.run(init)
+        for epoch in range(args.epochs):
+            self.sess.run(iterator.initializer)
+            batch_num = 1
+            running_cost = 0.0
 
-            step_num = 0
-            for epoch in range(args.epochs):
-                sess.run(iterator.initializer)
-                batch_num = 1
-                running_cost = 0.0
+            # Run through the epoch
+            while True:
+                try:
+                    # Note that each x, y pair in the batch has an epoch number
+                    batch_xs, batch_ys = self.sess.run(next_element)
 
-                # Run through the epoch
-                while True:
-                    try:
-                        # Note that each x, y pair in the batch has an epoch number
-                        batch_xs, batch_ys = sess.run(next_element)
+                except tf.errors.OutOfRangeError:
+                    print("End of training dataset.")
+                    break
 
-                    except tf.errors.OutOfRangeError:
-                        print("End of training dataset.")
-                        break
+                # Train using batch data
+                summary, c, _ = self.sess.run([merged, self.loss, train_step], feed_dict={self.x: batch_xs,
+                                                                                          self.y: batch_ys})
+                # Accumulate loss
+                running_cost += c
 
-                    # Train using batch data
-                    summary, c, _ = sess.run([merged, loss, train_step], feed_dict={self.x: batch_xs,
-                                                                                    self.y: batch_ys})
-                    # Accumulate loss
-                    running_cost += c
+                # Display log message every n batches
+                if batch_num % args.display_interval == 0:
+                    print("Epoch: %04d Batch: %06d cost=%.9f" % (epoch + 1, batch_num, running_cost / args.display_interval))
+                    running_cost = 0.0
 
-                    # Display log message every n batches
-                    if batch_num % args.display_interval == 0:
-                        print("Epoch: %04d Batch: %06d cost=%.9f" % (epoch + 1, batch_num, running_cost / args.display_interval))
-                        running_cost = 0.0
-
+                if step_num % args.display_interval == 0:
                     train_writer.add_summary(summary, step_num)
-                    batch_num += 1
-                    step_num += 1
+                batch_num += 1
+                step_num += 1
 
             print("Training Finished!")
             if args.save_dir:
-                save_path = saver.save(sess, args.save_dir)
-                print("Saved model to "+args.save_dir)
+                save_path = saver.save(self.sess, args.save_dir)
+                print("Saved model to "+save_path)
 
     def score(self, input_fn, args):
-        tf.reset_default_graph()
+        # Create a tf session if needed
+        if self.sess is None:
+            self.sess = tf.Session()
+
         dataset = input_fn()
-        self._get_widths(dataset)
-        self._build_model()
+
+        if self.loss is None:
+            # Prepare our model and acquire a reference to the loss
+            self.loss = self._build_model(self.config)
+
+            # Initialize all of the model variables (i.e. assign their default values)
+            init = tf.global_variables_initializer()
+            self.sess.run(init)
 
         dataset = dataset.apply(tf.contrib.data.batch_and_drop_remainder(args.batch_size))
         print("Dataset size: "+str(dataset.output_shapes))
@@ -147,38 +172,32 @@ class LogisticRegression:
         subtotal = 0
         total_tested = 0
 
-        with tf.Session() as sess:
-            if self.model_dir:
-                saver = tf.train.Saver()
-                saver.restore(sess, self.model_dir)
-                print("Model restored.")
+        batch_num = 0
 
-            batch_num = 0
+        # Build graph to calculate accuracy
+        label_class = tf.argmax(self.y, 1)
+        correct_preds = tf.equal(self.maxpreds, label_class)
+        correct = tf.reduce_sum(tf.cast(correct_preds, tf.float32))
 
-            # Build graph to calculate accuracy
-            label_class = tf.argmax(self.y, 1)
-            correct_preds = tf.equal(self.maxpreds, label_class)
-            correct = tf.reduce_sum(tf.cast(correct_preds, tf.float32))
+        import timeit
+        start = timeit.default_timer()
 
-            import timeit
-            start = timeit.default_timer()
+        while True:
+            try:
+                xs, ys = self.sess.run(next_element)
 
-            while True:
-                try:
-                    xs, ys = sess.run(next_element)
+                total_tested += xs.shape[0]
 
-                    total_tested += xs.shape[0]
+                subtotal += correct.eval({self.x: xs, self.y: ys})
 
-                    subtotal += correct.eval({self.x: xs, self.y: ys})
+                if batch_num % args.display_interval == 0:
+                    print("Batch: %06d %d/%d" % (batch_num, subtotal, total_tested))
 
-                    if batch_num % args.display_interval == 0:
-                        print("Batch: %06d %d/%d" % (batch_num, subtotal, total_tested))
+                batch_num += 1
 
-                    batch_num += 1
-
-                except tf.errors.OutOfRangeError:
-                    print("End of dataset.")
-                    break
+            except tf.errors.OutOfRangeError:
+                print("End of dataset.")
+                break
 
             stop = timeit.default_timer()
 
@@ -186,12 +205,8 @@ class LogisticRegression:
         print(stop - start)
 
     def predict(self, docs, num_classes=10):
-        tf.reset_default_graph()
-
         self.width_in = len(self.word_dict.keys())
         self.width_out = num_classes
-
-        self._build_model()
 
         inputs = np.zeros([len(docs), len(self.word_dict.keys())])
         for i, doc in enumerate(docs):
@@ -199,13 +214,24 @@ class LogisticRegression:
                 if word in self.word_dict:
                     inputs[i][j] = 1
 
-        with tf.Session() as sess:
-            saver = tf.train.Saver()
-            if self.model_dir:
-                saver.restore(sess, self.model_dir)
-                self.model_dir = None
-
-            label_classes = sess.run(self.maxpreds, feed_dict={self.x: inputs})
+        label_classes = self.sess.run(self.maxpreds, feed_dict={self.x: inputs})
 
         return label_classes
+
+    def close_sess(self):
+        if self.sess:
+            self.sess.close()
+            self.sess = None
+
+    def load_model(self, model_dir, config=None):
+        # Reset the current session and graph
+        self.close_sess()
+        tf.reset_default_graph()
+
+        # Construct
+        self.loss = self._build_model()
+        self.sess = tf.Session()
+        saver = tf.train.Saver()
+        saver.restore(self.sess, self.model_dir)
+        print("Model restored.")
 
